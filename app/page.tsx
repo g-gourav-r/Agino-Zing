7e9c0c940cf654f8d72cbca5f20b9bfc0ce57b26
Gourav Ravichandra <gourav.ravichandra@blenheimchalcot.com>
2025-02-27 13:35:42 +0530

    Change the text and background

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,28 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { ModeToggle } from "@/components/Toggle";
 import { LampContainer } from "@/components/ui/lamp";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="relative min-h-screen bg-gradient-to-r dark:to-gray-900 flex flex-col items-center justify-center">
-      <div className="absolute top-5 right-5">
-        <ModeToggle />
-      </div>
+    <div className="relative min-h-screen bg-gradient-to-br from-black to-gray-900 flex flex-col items-center justify-center">
       <LampContainer>
-        <h1 className="text-5xl font-bold mb-4 text-center">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-black via-gray-700 to-gray-900">
+        <h1 className="text-6xl font-extrabold mb-4 text-center">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-200 to-white">
             Agino
           </span>{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#28a745] to-[#6ddf7e]">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-600">
             Zing
           </span>
         </h1>
-        <p className="text-xl text-center mb-8">
-          Your quick and fast single use application
+        <p className="text-lg text-center text-gray-300 mb-8">
+          Experience lightning-fast, intuitive solutions at your fingertips.
         </p>
-
         <button
-          className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           onClick={() => router.push("/upload")}
+          className="inline-flex items-center justify-center h-14 px-8 font-bold text-white bg-gradient-to-r from-green-400 to-green-600 rounded-full shadow-lg transition-transform duration-300 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-500"
         >
           Get Started
         </button>
